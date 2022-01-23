f4fea139a72c2c1a0d819d25f3e9dc5c3cf67bde
Matthew Kehrt <mkehrt@roseandsigil.net>
2022-01-22 21:58:36 -0500

    Redo a lot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use glfw::{Context as _, WindowEvent};
 use luminance_glfw::GlfwSurface;
+use luminance::context::GraphicsContext as _;
+use luminance::pipeline::PipelineState;
 use luminance_windowing::{WindowDim, WindowOpt};
 use std::sync::mpsc;
 
@@ -18,27 +20,43 @@ fn make_surface() -> GlfwSurface {
     surface.unwrap()
 }
 
-fn run(mut surface: GlfwSurface) {
-    'app: loop {
-        surface.context.window.glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&surface.events_rx) {
-            if !handle_event(event) {
-                break 'app;
-            };
-        }
+fn run(mut surface: GlfwSurface) -> Result<(), ()> {
+    loop {
+        handle_event(&mut surface)?;
+        render(&mut surface)?;
         surface.context.window.swap_buffers();
     }
 }
 
-fn handle_event(event: WindowEvent) -> bool {
-    match event {
-        WindowEvent::Close => false,
-        _ => true,
+fn handle_event(surface: &mut GlfwSurface) -> Result<(), ()> {
+    surface.context.window.glfw.poll_events();
+    for (_, event) in glfw::flush_messages(&surface.events_rx) {
+        match event {
+            WindowEvent::Close => Err(())?,
+            _ => (),
+        }
     }
+
+    Ok(())
+}
+
+fn render(surface: &mut GlfwSurface) -> Result<(), ()> {
+    let color = [1.0, 1.0, 0.0, 1.0];
+
+    let back_buffer = surface.context.back_buffer().unwrap();
+
+    surface.context
+    .new_pipeline_gate()
+    .pipeline(
+      &back_buffer,
+      &PipelineState::default().set_clear_color(color),
+      |_, _| Ok(()),
+    )
+    .assume();
+    Ok(())
 }
 
 fn main() {
     let surface = make_surface();
-
-    run(surface)
+    let _ = run(surface);
 }
