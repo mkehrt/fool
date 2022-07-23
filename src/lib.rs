cf113bb3d181d5a29fdfbbb880462b882594bb5e
Matthew Kehrt <mkehrt@roseandsigil.net>
2022-07-23 17:15:10 -0400

    js stuff

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,9 @@ use wasm_bindgen::JsCast;
 static WIDTH: i32 = 640;
 static HEIGHT: i32 = 480;
 
-#[wasm_bindgen(start)]
-pub fn start() {
+mod geometry;
+
+fn get_context() -> web_sys::CanvasRenderingContext2d {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas
@@ -21,28 +22,12 @@ pub fn start() {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    context.begin_path();
-
-    // Draw the outer circle.
     context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
+}
 
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
+#[wasm_bindgen(main)]
+pub fn main() {
+    println!("AAAAAAA");
+    let mut context = get_context();
+    geometry::draw(&mut context);
 }
