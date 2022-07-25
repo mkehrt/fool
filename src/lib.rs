a0599df898624a0e5303824203f839aef8fcd18e
Matthew Kehrt <mkehrt@roseandsigil.net>
2022-07-24 22:53:40 -0400

    progress in making tree, drawing

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
-use std::f64;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
 static WIDTH: i32 = 640;
 static HEIGHT: i32 = 480;
 
-mod geometry;
+mod color;
+mod element;
 
-fn get_context() -> web_sys::CanvasRenderingContext2d {
+use color::Color;
+use element::{Element, Parent};
+
+type Context = web_sys::CanvasRenderingContext2d;
+
+fn get_context() -> Context {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
     let canvas: web_sys::HtmlCanvasElement = canvas
@@ -25,9 +30,33 @@ fn get_context() -> web_sys::CanvasRenderingContext2d {
     context
 }
 
+fn set_initial_transform(context: &mut Context) {
+    context.translate(150.0, 150.0);
+    context.scale(-20.0, -20.0);
+}
+
 #[wasm_bindgen]
 pub fn start() {
-    println!("AAAAAAA");
     let mut context = get_context();
-    geometry::draw(&mut context);
+
+    let root = Element {
+        distal_color: Color {
+            red: 255,
+            green: 0,
+            blue: 255,
+        },
+        height: 1.0,
+        angle: 0.0,
+        children: Vec::new(),
+    };
+    let root_parent = Parent {
+        color: Color {
+            red: 255,
+            green: 0,
+            blue: 255,
+        },
+    };
+
+    set_initial_transform(&mut context);
+    root.draw(&root_parent, &mut context);
 }
