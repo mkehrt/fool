04e7f658d5af39addd95bb171147548415c0c8f3
Matthew Kehrt <mkehrt@roseandsigil.net>
2023-05-10 21:42:43 -0400

    Start fool

--- src/draw.rs
+++ src/draw.rs
@@ -2,11 +2,13 @@ use crate::Context;
 
 use rand::Rng;
 
+mod fool;
 mod rock;
 mod sky;
 mod star;
 
 pub fn draw<R: Rng>(context: &mut Context, rng: &mut R) {
     sky::draw(context, rng);
-    rock::draw(context);
+    // rock::draw(context);
+    fool::draw(context);
 }
