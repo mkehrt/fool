58fe6535438a6b6f869d32fd5f4fe3031a43b000
Matthew Kehrt <mkehrt@roseandsigil.net>
2023-05-28 23:31:18 -0400

    First draft of drawing fool

--- src/draw/fool.rs
+++ src/draw/fool.rs
@@ -17,6 +17,35 @@ fn nodes() -> Vec<Node<'static>> {
         Node("AdamsApple", 3.5, 2.25),
         Node("Nape", 2.6, 2.2),
         Node("HeadBack", 1.5, 1.2),
+        Node("NeckBack", 3.0, 3.1),
+        Node("NeckFront", 3.8, 2.7),
+        Node("FrontShoulder", 5.0, 2.0),
+        Node("FrontInnerElbow", 7.0, 0.0),
+        Node("FrontHand", 9.0, -3.8),
+        Node("FrontElbow", 7.5, 0.4),
+        Node("FrontArmpit", 5.5, 3.0),
+        Node("FrontWaist", 5.0, 8.2),
+        Node("BackShoulder", 2.0, 3.6),
+        Node("BackElbow", 0.3, 6.8),
+        Node("BackHand", -0.5, 10.65),
+        Node("BackInnerElbow", 1.0, 7.0),
+        Node("BackArmpit", 2.5, 4.9),
+        Node("BackWaist", 3.0, 8.0),
+        Node("BackWaist", 3.0, 8.0),
+        Node("FrontWaist", 5.0, 8.2),
+        Node("Groin", 6.5, 9.5),
+        Node("FrontKnee", 10.0, 12.0),
+        Node("FrontAnkle", 8.0, 16.4),
+        Node("FrontToes", 9.0, 17.0),
+        Node("FrontHeel", 7.0, 17.0),
+        Node("FrontInnerKnee", 8.0, 13.0),
+        Node("Root", 4.5, 11.5),
+        Node("BackKnee", 3.2, 15.8),
+        Node("BackAnkle", 1.0, 19.0),
+        Node("BackToes", 1.4, 20.4),
+        Node("BackHeel", 0.2, 19.0),
+        Node("BackInnerKnee", 2.0, 15.8),
+        Node("Butt", 2.0, 10.2),
     ]
 }
 
@@ -25,8 +54,37 @@ fn edges() -> Vec<Edge<'static>> {
         Edge("HeadTop", "Forehead"),
         Edge("Forehead", "Chin"),
         Edge("Chin", "AdamsApple"),
+        Edge("NeckFront", "AdamsApple"),
         Edge("Nape", "HeadBack"),
         Edge("HeadBack", "HeadTop"),
+        Edge("HeadBack", "Nape"),
+        Edge("Nape", "NeckBack"),
+        Edge("NeckBack", "BackShoulder"),
+        Edge("BackShoulder", "BackElbow"),
+        Edge("BackElbow", "BackHand"),
+        Edge("BackHand", "BackInnerElbow"),
+        Edge("BackInnerElbow", "BackArmpit"),
+        Edge("BackArmpit", "BackWaist"),
+        Edge("NeckFront", "FrontShoulder"),
+        Edge("FrontShoulder", "FrontInnerElbow"),
+        Edge("FrontInnerElbow", "FrontHand"),
+        Edge("FrontHand", "FrontElbow"),
+        Edge("FrontElbow", "FrontArmpit"),
+        Edge("FrontArmpit", "FrontWaist"),
+        Edge("FrontWaist", "Groin"),
+        Edge("Groin", "FrontKnee"),
+        Edge("FrontKnee", "FrontAnkle"),
+        Edge("FrontAnkle", "FrontToes"),
+        Edge("FrontToes", "FrontHeel"),
+        Edge("FrontHeel", "FrontInnerKnee"),
+        Edge("FrontInnerKnee", "Root"),
+        Edge("Root", "BackKnee"),
+        Edge("BackKnee", "BackAnkle"),
+        Edge("BackAnkle", "BackToes"),
+        Edge("BackToes", "BackHeel"),
+        Edge("BackHeel", "BackInnerKnee"),
+        Edge("BackInnerKnee", "Butt"),
+        Edge("Butt", "BackWaist"),
     ]
 }
 
@@ -34,7 +92,7 @@ fn node_for_name(name_to_find: &str) -> Node {
     nodes()
         .into_iter()
         .find(|Node(name, _x, _y)| &name_to_find == name)
-        .expect("Couldn't find node.")
+        .expect(format!("Couldn't find node {:}.", name_to_find).as_str())
 }
 
 pub fn draw(context: &mut Context) {
