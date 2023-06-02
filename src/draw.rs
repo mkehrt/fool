3b913ef311ee9ce776134216dd74011f834b0ec1
Matthew Kehrt <mkehrt@roseandsigil.net>
2023-06-02 01:34:01 -0400

    Fix margins

--- src/draw.rs
+++ src/draw.rs
@@ -10,6 +10,8 @@ mod star;
 mod stars;
 mod text;
 
+const MARGIN_IN_PIXELS: f64 = 36.0;
+
 fn set_initial_transform(context: &mut Context) {
     let _ = context.scale(UNIT_IN_PIXELS, UNIT_IN_PIXELS);
 }
@@ -27,35 +29,61 @@ pub fn draw<R: Rng>(context: &mut Context, mut rng: R) {
 
     context.restore();
 
-    // draw_margins(context, true)
+    draw_margins(context, true)
 }
 
+#[allow(unused)]
 fn draw_margins(context: &mut Context, with_inner: bool) {
     let margin_color = "rgba(255, 255, 255, 0.75)";
     context.set_fill_style(&margin_color.into());
 
-    context.fill_rect(0.0, 0.0, 36.0, HEIGHT_IN_PIXELS);
-    context.fill_rect(0.0, 0.0, WIDTH_IN_PIXELS, 36.0);
-    context.fill_rect(WIDTH_IN_PIXELS, HEIGHT_IN_PIXELS, -36.0, -HEIGHT_IN_PIXELS);
-    context.fill_rect(WIDTH_IN_PIXELS, HEIGHT_IN_PIXELS, -WIDTH_IN_PIXELS, -36.0);
+    context.fill_rect(0.0, 0.0, WIDTH_IN_PIXELS, MARGIN_IN_PIXELS);
+    context.fill_rect(
+        0.0,
+        HEIGHT_IN_PIXELS - MARGIN_IN_PIXELS,
+        WIDTH_IN_PIXELS,
+        MARGIN_IN_PIXELS,
+    );
+    context.fill_rect(
+        0.0,
+        MARGIN_IN_PIXELS,
+        MARGIN_IN_PIXELS,
+        HEIGHT_IN_PIXELS - 2.0 * MARGIN_IN_PIXELS,
+    );
+    context.fill_rect(
+        WIDTH_IN_PIXELS - MARGIN_IN_PIXELS,
+        MARGIN_IN_PIXELS,
+        MARGIN_IN_PIXELS,
+        HEIGHT_IN_PIXELS - 2.0 * MARGIN_IN_PIXELS,
+    );
 
     if (with_inner) {
         let inner_margin_color = "rgba(255, 255, 255, 0.25)";
         context.set_fill_style(&inner_margin_color.into());
 
-        context.fill_rect(36.0, 0.0, 36.0, HEIGHT_IN_PIXELS);
-        context.fill_rect(0.0, 36.0, WIDTH_IN_PIXELS, 36.0);
         context.fill_rect(
-            WIDTH_IN_PIXELS - 36.0,
-            HEIGHT_IN_PIXELS,
-            -36.0,
-            -HEIGHT_IN_PIXELS,
+            MARGIN_IN_PIXELS,
+            MARGIN_IN_PIXELS,
+            WIDTH_IN_PIXELS - 2.0 * MARGIN_IN_PIXELS,
+            MARGIN_IN_PIXELS,
         );
         context.fill_rect(
-            WIDTH_IN_PIXELS,
-            HEIGHT_IN_PIXELS - 36.0,
-            -WIDTH_IN_PIXELS,
-            -36.0,
+            MARGIN_IN_PIXELS,
+            HEIGHT_IN_PIXELS - 2.0 * MARGIN_IN_PIXELS,
+            WIDTH_IN_PIXELS - 2.0 * MARGIN_IN_PIXELS,
+            MARGIN_IN_PIXELS,
+        );
+        context.fill_rect(
+            MARGIN_IN_PIXELS,
+            2.0 * MARGIN_IN_PIXELS,
+            MARGIN_IN_PIXELS,
+            HEIGHT_IN_PIXELS - 4.0 * MARGIN_IN_PIXELS,
+        );
+        context.fill_rect(
+            WIDTH_IN_PIXELS - 2.0 * MARGIN_IN_PIXELS,
+            2.0 * MARGIN_IN_PIXELS,
+            MARGIN_IN_PIXELS,
+            HEIGHT_IN_PIXELS - 4.0 * MARGIN_IN_PIXELS,
         );
     }
 }
